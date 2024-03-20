4214944192372282ef3f38cb95de7fafcbf910ed
Gaudencio Jurado <gjurado@virtualidevs.com>
2024-03-19 19:38:20 -0600

    missing the api part

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const apiBaseUrl = process.env.BACKEND_API_URL || 'https://whale-app-qtzuz.ondigitalocean.app/';
+const apiBaseUrl = process.env.BACKEND_API_URL || 'https://whale-app-qtzuz.ondigitalocean.app/api';
 
 export { apiBaseUrl };
